26879e838f10d3440047f4eae90b6ffd399038f9
Lucas Da Silva Stanley <108637467+LukStanley@users.noreply.github.com>
2022-08-13 21:34:24 -0300

    funções para definir se o usuário é maior de idade

---
+++ js/validacao.js
@@ -0,0 +1,32 @@
+export function valida(input){
+    const input = input.dataset.tipo
+
+    if (validadores[tiposDeInput]){
+        validadores[tiposDeInput](input)
+    }
+}
+
+const validadores = {
+    dataNascimento: input => validaDataNascimento(input)
+}
+
+function validaDataNascimento(input){
+    const dataRecebida = new Date(input.value);
+    let mensagem = ''
+
+   if(!maiorQue18(dataRecebida)){
+        mensagem = 'Para se cadastrar você deve ser maior de idade';
+
+   };
+
+   input.setCustomValidity(mensagem)
+}
+
+function maiorQue18(data){
+
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+
+    return dataMais18 <= dataAtual;
+
+}
