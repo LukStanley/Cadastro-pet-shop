dd30f63e4fbeda66c6cd70911fe5df6cfb5065f5
LukStanley <lukksterror.com>
2022-09-01 21:22:07 -0300

    novos arquivos do projeto

--- js/validacao.js
+++ js/validacao.js
@@ -7,7 +7,30 @@ export function valida(input){
 
     if (input.validity.valid){
             input.parentElement.classList.remove('input-container--invalido')
+    }else{
+            input.parentElement.classList.add('input-container--invalido')
     }
+}
+
+const mensagemDeErro = {
+    nome:{
+        valueMissing: 'O campo nome não pode estar vazio.'
+    },
+    email: {
+        valueMissing: 'O campo email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é válido.'
+    },
+    senha:{ 
+        valueMissing: 'O campo de senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter 6 a 12 caracteres, pelo menos uma letra maiúscula, um número e não deve conter símbolos'
+    },  
+    dataNascimento: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError:  'Você deve ter mais de 18 anos para efetuar o cadastro.'
+    }  
+
+
+
 }
 
 const validadores = {
