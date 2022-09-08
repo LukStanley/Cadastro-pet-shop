f1292d7c442e588ee59ab40189fce9003cc19ad0
LukStanley <lukksterror.com>
2022-09-07 21:45:25 -0300

    utilizando API para validar o CEP

--- js/validacao.js
+++ js/validacao.js
@@ -55,15 +55,23 @@ const mensagensDeErro = {
     cep: {
         valueMissing: 'O campo de CEP não pode estar vazio.',
         patternMismatch: 'O CEP digitado não é válido.'
+    },
+    logradouro:{
+        valueMissing: 'O campo de logradouro não pode estar vazio.'
+    },
+    cidade:{
+        valueMissing: 'O campo de cidade não pode estar vazio.'
+    },
+    estado:{
+        valueMissing: 'O campo de estado não pode estar vazio.'
     }
-
-
 
 }
 
 const validadores = {
     dataNascimento: input => validaDataNascimento(input),
-    cpf: input => validaCpf(input)
+    cpf: input => validaCpf(input),
+    cep: input => recuperarCEP(input)
 }
 
 function mostraMensagemDeErro(tiposDeInput, input) {
@@ -169,3 +177,25 @@ function confirmaDigito(soma){
     return 11 - (soma % 11)
 }
 
+function recuperarCEP(input){
+    const cep = input.value.replace(/\D/g, '')
+    const url = 'https://viacep.com.br/ws/${cep}/json/'
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'content-type': 'application/json;charset=utf-8'
+        }
+    }
+
+    if(!input.validity.patternMismatch && !input.validity.valueMissing){
+        fetch(url, options).then(
+            response => response.json()
+            ).then(
+                data => {
+                    console.log(data)
+                })
+        
+    }
+
+}
